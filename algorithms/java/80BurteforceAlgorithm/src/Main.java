7d952bd60d3fdc3483c349148520680dab405456
Christoph Hartleb (Dev) <c.hartleb@vie.altova.com>
2024-04-24 14:03:24 +0200

    Updated java algorithms

--- algorithms/java/80BurteforceAlgorithm/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        char[] charArray = {'a','b','a','b','a','a','b','a','b'};
-        char[] charPattern = {'a','b'};
-        BruteForce brfrce = new BruteForce();
-        brfrce.firstMatch(charArray, charPattern);
-        brfrce.everyMatch(charArray, charPattern);
-    }
-}
